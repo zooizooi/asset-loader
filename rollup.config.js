780d3cc0612fa6c9545d6b3c447f56f23f56267e
ZOOI <zooii.kut@gmail.com>
2024-05-27 19:53:30 -0300

    :wrench: Change to better TypeScript transpiler

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import typescript from '@rollup/plugin-typescript';
+import { dts } from 'rollup-plugin-dts';
+
+export default [
+    {
+        input: 'lib/AssetLoader.ts',
+        output: [
+            {
+                name: 'AssetLoader',
+                file: 'dist/AssetLoader.js',
+                format: 'es',
+            },
+        ],
+
+        external: ['three'],
+        plugins: [typescript()],
+    },
+    {
+        input: 'lib/AssetLoader.ts',
+        output: [{
+            file: 'dist/AssetLoader.d.ts',
+            format: 'es',
+        }],
+        plugins: [dts()],
+    },
+];
